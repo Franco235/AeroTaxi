f3b71c75ecef88162013282bdd8ab82d7084774e
Franco235 <57547815+Franco235@users.noreply.github.com>
2020-06-06 12:46:45 -0300

    Modificaciones a las clases

--- src/usuario/Bronze.java
+++
@@ -1,7 +0,0 @@
-package usuario;
-
-public class Bronze extends Avion{
-
-    public Bronze(int capacidadComb, int costoKm, int capacidadPasajeros, int velocidadMax, String propulsor) {
-        super(capacidadComb, costoKm, capacidadPasajeros, velocidadMax, propulsor);
-}
